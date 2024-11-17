fbf24efcdd8f37978ee621149ececb4b9bd42ec4
saravana3252 <sara18ec118@gmail.com>
2024-11-17 10:11:10 +0530

    alert added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let parentDiv=document.getElementById("parentDiv");
 if(randomNumber===guessedNumber){
     parentDiv.style.backgroundColor="green";
     ramdomData.innerHTML=`<h1>YOU WON!! THE NUMBER WAS ${randomNumber}</h1>`;
+    alert(`YOU WON!! THE NUMBER WAS ${randomNumber}`)
 }
 else {
     ramdomData.innerHTML=`<h1>YOU LOSE!! THE NUMBER WAS ${randomNumber}</h1>`;
