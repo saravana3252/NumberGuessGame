eb7f8d4c42b7c1a75dc8f14f6adba47838e9775c
saravana3252 <sara18ec118@gmail.com>
2024-11-26 10:19:23 +0530

    guess attempt added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 // }
 
 
+let attempt=0;
+let maxAttempt=3;
 
 function checkAns(){
 let randomNumber=Math.floor(Math.random()*10)+1;
@@ -15,15 +17,27 @@ let ramdomData=document.getElementById("pData");
 let parentDiv=document.getElementById("parentDiv");
 let btnReset=document.getElementById("btn-Reset");
 
+attempt++;
+
+if(randomNumber>5){
+    randomNumber=randomNumber-5;
+}
+
 if(randomNumber===guessedNumber){
     parentDiv.style.backgroundColor="green";
     ramdomData.innerHTML=`<h1>🎉YOU WON!! THE NUMBER WAS ${randomNumber}</h1>`;
+    attempt=0;
     setTimeout(()=>{alert(`YOU WON!!🎉 THE NUMBER WAS ${randomNumber}`),2000})
 }
 else {
-    ramdomData.innerHTML=`<h1>👎YOU LOSE!! THE NUMBER WAS ${randomNumber}</h1>`;
+    if(attempt<maxAttempt){
+    ramdomData.innerHTML=`<h1>👎WRONG GUESS!! ${maxAttempt - attempt} attempts left</h1>`;
+    }
+    else{
     parentDiv.style.backgroundColor="red";
+     ramdomData.innerHTML=`<h1>👎YOU LOSE!! THE NUMBER WAS ${randomNumber}</h1>`;
     btnReset.style.display="flex";
+    }
 }
 
 }
@@ -34,6 +48,7 @@ function ResetAns(){
     document.getElementById("pData").innerHTML="";
     document.getElementById("parentDiv").style.backgroundColor="rgb(30, 64, 175)";
     document.getElementById("btn-Reset").style.display="none";
+    attempt=0;
 
 }
 
